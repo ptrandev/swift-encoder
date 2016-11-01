f70c1f0de670ee956d698cc89fafbf80d5e87689
DonutDeflector <donutdeflector@tuta.io>
2016-10-31 20:01:55 -0400

    Added: completed encodes moved to new folder

--- swift-ffmpeg.sh
+++ swift-ffmpeg.sh
@@ -16,22 +16,22 @@
 # Video Codec
 # (libx264 or libx265)
 # default: libx265
-videocodec=libx265
+videocodec=libx264
 
 # Rate Factor
 # (0-51 | Lower Number = Higher Quality) 
 # default: 21
-ratefactor=21
+ratefactor=51
 
 # Preset
 # (ultrafast, superfast, veryfast, fast, medium, slow, slower, veryslow)
 # default: medium
-preset=medium
+preset=ultrafast
 
 # Pixel Format (chroma sampling and bit depth)
 # (yuv420p [8bit], yuv420p10le [10bit])
 # Default: yu4v2010le
-pixelformat=yuv420p10le
+pixelformat=yuv420p
 
 # Audio Codec
 # (ac3, eac3, wmav1, wmav2, libmp3lame, libfdk_aac, aac, libvorbis, vorbis, libopus)
@@ -63,11 +63,26 @@ inputformat=mkv
 # default: mkv
 outputformat=mp4
 
+# Completed Files Folder
+# (Ex: Completed | quality21/completed)
+# default: Completed
+
 #########################
 
 # Core Functionality
 # (Better off not touching unless you know what you are doing)
-for f in "$1"/*."$inputformat"; do ffmpeg -i "$f" -c:v "$videocodec" \
-         -crf "$ratefactor" -preset "$preset" -c:a "$audiocodec" \
-         -"$audioencoding" "$audiobitrate" -ac "$audiochannels" -pix_fmt "$pixelformat" \
+for f in "$1"/*."$inputformat"; do 
+         ffmpeg -i "$f" \
+         -c:v "$videocodec" \
+         -crf "$ratefactor" \
+         -preset "$preset" \
+         -c:a "$audiocodec" \
+         -"$audioencoding" "$audiobitrate" \
+         -ac "$audiochannels" \
+         -pix_fmt "$pixelformat" \
          "${f%."$inputformat"}[sf]."$outputformat""; done
+
+# Palace of Immigration
+# This is where we move the files
+mkdir "$1""Completed"/ # Create the Completed Files Folder
+mv "$1"*"[sf]"."$outputformat" "$1""Completed"/
